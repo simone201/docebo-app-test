20002655c3c37129980edc5505388dfdd5e9e180
Simone Renzo <simone.renzo@docomodigital.com>
2019-10-04 10:13:55 +0200

    Base styling

--- app/src/main/java/it/simonerenzo/docebotest/MainActivity.kt
+++ app/src/main/java/it/simonerenzo/docebotest/MainActivity.kt
@@ -2,6 +2,7 @@ package it.simonerenzo.docebotest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.mikepenz.materialize.MaterializeBuilder
 import it.simonerenzo.docebotest.ui.fragment.SearchFragment
 
 class MainActivity : AppCompatActivity() {
@@ -9,6 +10,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
+
+        MaterializeBuilder().withActivity(this).build()
+
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.container, SearchFragment.newInstance())
